fb51e5e5645dda81d95aac8c9ec92ac89ccc927f
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-11 11:14:51 +0200

    Rafactor a tag to link

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
     </Route>
   )
 );
